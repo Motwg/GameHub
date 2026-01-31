dc1488735ca023706c3d40a7324f2e5342d61fdc
Motwg <piruci88@gmail.com>
2026-01-31 15:30:16 +0100

    Update errorhandlers.py

--- website/gamehub/errorhandlers.py
+++ website/gamehub/errorhandlers.py
@@ -2,9 +2,9 @@ from flask import Response, g, render_template
 
 
 # error handlers
-def error_404(e: Exception) -> Response:
+def error_404(_: Exception) -> Response:
     return Response(render_template('error-pages/error404.html', mc='', g=g), status=404)
 
 
-def error_500(e: Exception) -> Response:
+def error_500(_: Exception) -> Response:
     return Response(render_template('error-pages/error500.html', mc='', g=g), status=500)
